8dc294c70282e586b1cb7d57a6c8f0e3b79b30cc
Marcus <marcus@deltalima.org>
2023-05-01 03:58:47 +0200

    progress

--- makeanything.sh
+++ makeanything.sh
@@ -4,3 +4,22 @@
 ###
 ### By: DeltaLima
 ### 2023
+
+message () {
+  echo "== " $1
+}
+
+error () 
+{
+  message "ERROR!!"
+  exit 1
+}
+
+check_requirements () {
+  echo "we are checking for requirements"
+}
+
+### stuff begins here
+test -f build/chroot || mkdir -p build/chroot
+
+debootstrap --variant=fakeroot bullseye build/chroot/ http://ftp.de.debian.org/debian
